65f790c768f4a330b74e70927c6d89132f9dbcf4
hmk <skdfw12@gmail.com>
2025-11-24 12:24:15 +0900

    init commit

---
+++ app/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../mathlib/my_math.hpp"
+
+int main(int, char**)
+{
+    std::cout << "Hello, from my_test!\n";
+    std::cout << add(1,2) << std::endl;
+
+    return 0;
+}
