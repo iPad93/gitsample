e248983e6bea963ab73610a8e659497208d6e54b
hmk <skdfw12@gmail.com>
2025-11-24 12:27:54 +0900

    second commit

---
+++ build/mathlib/CMakeFiles/mathlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathlib.dir/my_math.cpp.o"
+  "CMakeFiles/mathlib.dir/my_math.cpp.o.d"
+  "libmathlib.pdb"
+  "libmathlib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mathlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
